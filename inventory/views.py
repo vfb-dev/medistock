009e1871160beef255a9c0ac454b2f827ceca434
Victor <victorfariasbezerra12@gmail.com>
2026-06-27 22:21:55 -0300

    dashboard page

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Medicine, Supplier, StockBatch, StockMovement
+
+def dashboard(request):
+    total_medicines = Medicine.objects.count()
+    total_suppliers = Supplier.objects.count()
+    total_batches = StockBatch.objects.count()
+    recent_movements = StockMovement.objects.order_by('-created_at')[:5]
+
+    context = {
+        'total_medicines': total_medicines,
+        'total_suppliers': total_suppliers,
+        'total_batches': total_batches,
+        'recent_movements': recent_movements,
+    }
+
+    return render(request, 'inventory/dashboard.html', context)
